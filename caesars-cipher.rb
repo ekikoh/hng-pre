d6e6d5ac5945802f5d32463c2c7533c7f163b188
revealtheweb <aniudohr@gmail.com>
2021-02-24 10:48:41 +0100

    fix: respect spaces

--- caesars-cipher.rb
+++ caesars-cipher.rb
@@ -2,8 +2,11 @@ def caesar_crypt(string, shift)
   alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('')
   encrypted_word = []
   param_string = string.split('')
-
+  # puts param_string
   for a in param_string do
+    if a == " "
+      encrypted_word.push(" ")
+    end
     if alphabets.include?(a)
       if alphabets.find_index(a) >= 25
         shifted = shift - 1
