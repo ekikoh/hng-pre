7abdb5529566d24c52db1cb3fcd1dbbe609fbca5
revealtheweb <aniudohr@gmail.com>
2021-02-24 11:30:10 +0100

    fix: improvment?

--- caesars-cipher.rb
+++ caesars-cipher.rb
@@ -1,13 +1,15 @@
 def caesar_crypt(string, shift)
   alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('')
   encrypted_word = []
-  param_string = string.split('')
-  # puts param_string
+  param_string = string.downcase.split('')
+
   for a in param_string do
+    # if spaces in text, respect the spaces
     if a == " "
       encrypted_word.push(" ")
     end
     if alphabets.include?(a)
+      # wrap when it gets to end of alphabets
       if alphabets.find_index(a) >= 25
         shifted = shift - 1
       else
@@ -16,8 +18,8 @@ def caesar_crypt(string, shift)
       encrypted_word.push(alphabets[shifted])
     end
   end
-  return encrypted_word.join('')
+  encrypted_word.join('')
 
 end
 
-puts caesar_crypt('what a string', 5)
+puts caesar_crypt('what a string', 3)
