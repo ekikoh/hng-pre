c289c041eeaa348f0a19999dd7c19bfb1b4397aa
revealtheweb <aniudohr@gmail.com>
2021-02-24 09:32:23 +0100

    first try: caesar's cipher

---
+++ caesars-cipher.rb
@@ -0,0 +1,20 @@
+def caesar_crypt(string, shift)
+  alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  encrypted_word = []
+  param_string = string.split('')
+
+  for a in param_string do
+    if alphabets.include?(a)
+      if alphabets.find_index(a) >= 25
+        shifted = shift - 1
+      else
+        shifted = alphabets.find_index(a) + shift
+      end
+      encrypted_word.push(alphabets[shifted])
+    end
+  end
+  return encrypted_word.join('')
+
+end
+
+puts caesar_crypt('what a string', 5)
